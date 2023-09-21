6d7e04d50f1ed5141607fe41be3b17f3ca783186
YONROMERO <yonfrv@gmail.com>
2023-09-21 17:06:53 -0500

    Update

--- src/main/java/com/seeri/challenge/controller/TaskController.java
+++ src/main/java/com/seeri/challenge/controller/TaskController.java
@@ -11,10 +11,11 @@ import com.seeri.challenge.service.taskService.putTaskService.IPutTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@RestController
+@Controller
 @CrossOrigin(origins = {"*"})
 @RequestMapping("api/v1")
 public class TaskController {
